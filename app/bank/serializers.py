13b5712c61e9bf2b54ab539a8f0edafdd57fc007
geekwise-maria-navarro <geekwise.maria.navarro@gmail.com>
2019-12-10 04:36:43 +0000

    create: `serializer.py and views.py` files

---
+++ app/bank/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from .models import Branch, Customer, Product
+
+
+
+class Branch_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branch
+        fields = (
+            'branch_name',
+            'branch_location'
+        )
+
+class Customer_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = (
+            'customer_name',
+            'customer_email'
+        )
+
+class Product_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'product_options'
+        )
+
+
+# class Todo_Serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Todo
+#         fields = ('id', 'title', 'description', 'completed')
--- app/bank/views.py
+++ app/bank/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
 
 # Create your views here.
+
+
+
+
+# from django.shortcuts import render
+# from rest_framework import viewsets
+# from .serializers import Todo_Serializer
+# from .models import Todo
+
+# # Create your views here.
+
+# class TodoView(viewsets.ModelViewSet):
+#     serializer_class = Todo_Serializer
+#     queryset = Todo.objects.all()
