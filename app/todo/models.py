f437cfb5a3ae65889bbfa5c8ffc402aebf9c941f
geekwise-maria-navarro <geekwise.maria.navarro@gmail.com>
2020-01-02 13:44:31 -0800

    update: updated branch model

--- app/todo/models.py
+++ app/todo/models.py
@@ -6,11 +6,11 @@ from django.db import models
 
 class Branch(models.Model):
     branch = models.CharField(max_length=120)
-    customer = models.CharField(max_length=120)
+    branch_address = models.CharField(max_length=None)
     product_options = models.CharField(max_length=120)
 
     def __str__(self):
-        return self.customer
+        return self.branch
 class Customer(models.Model):
     customer_name = models.CharField(max_length=120)
     
