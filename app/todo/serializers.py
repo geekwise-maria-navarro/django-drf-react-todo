2f2616cda216c8b038bc8c5a524a0d2613ab0b88
geekwise-maria-navarro <geekwise.maria.navarro@gmail.com>
2020-01-13 16:15:57 -0800

    staging for deployment: deleted backend files

--- app/todo/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import BranchApp
-
-
-class Bank_Serializer(serializers.ModelSerializer):
-    class Meta:
-        model = BranchApp
-        fields = ('id', 'branch', 'address', 'product_options')
