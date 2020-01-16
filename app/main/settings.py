707a11318d5395cb746c99bdfc5a488651541827
geekwise-maria-navarro <geekwise.maria.navarro@gmail.com>
2020-01-15 16:02:26 -0800

    update: cors

--- app/main/settings.py
+++ app/main/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
-CORS_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 # (
 #     'https://maria-staging-frontend.herokuapp.com',
 # )
