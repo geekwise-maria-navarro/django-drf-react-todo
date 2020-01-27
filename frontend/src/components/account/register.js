a45e4560314e3b19ae77dcd6bbe52c4579af7912
geekwise-maria-navarro <geekwise.maria.navarro@gmail.com>
2020-01-27 15:08:47 -0800

    feature: added register drop down feature in `register.js`

--- frontend/src/components/account/register.js
+++ frontend/src/components/account/register.js
@@ -13,9 +13,9 @@ export class Register extends Component {
         password: "",
         justRegister: false,
         justRegisterUser: false,
-        // groups: [1],
-        // groupList: [],
-        // groupName: ""
+        groups: [1],
+        groupList: [],
+        groupName: ""
     };
 
     static propTypes = {
@@ -25,7 +25,60 @@ export class Register extends Component {
     
     componentDidMount() {
         this.setState({justRegister: false});
-    }
+        this.setState({justRegister: false});
+        this.getGroupList();
+    };
+
+    getGroupList() {
+      axios
+        .get('http://127.0.0.1:8000/api/group/')
+        .then( res => {
+          if(this.props.auth.user != null){
+            this.setState({ groupName: this.props.auth.user.groups[0].name });
+          }
+          this.setState({ groupList: res.data });
+          console.log(this.state.groupList);
+        })
+        .catch(err => console.log(err));
+    };
+
+  renderGroupOptions() {
+    // if(!this.props.isAuthenticated || this.state.groupName === "bank.admin") {
+    //   return this.state.groupList.slice(0, 5).map(group => (
+    //     <option key={group.id} value={group.id}>{group.name}</option>
+    //   ));
+    // }
+    // else if(this.state.groupName === "member") {
+    //   return this.state.groupList.slice(0, 5).map(group => (
+    //     <option key={group.id} value={group.id}>{group.name}</option>
+    //   ));
+    // }
+    // else if(this.state.groupName === "branch.admin") {
+    //   return this.state.groupList.slice(0, 5).map(group => (
+    //     <option key={group.id} value={group.id}>{group.name}</option>
+    //   ));
+    // }
+    // else if(this.state.groupName === "branch.staff") {
+    //   return this.state.groupList.slice(0, 5).map(group => (
+    //     <option key={group.id} value={group.id}>{group.name}</option>
+    //   ));
+    // }
+    // else {
+    //   return this.state.groupList.map(group => (
+    //     <option key={group.id} value={group.id}>{group.name}</option>
+    //   ));
+    // }
+    return this.state.groupList.map(group => (
+          <option key={group.id} value={group.id}>{group.name}</option>
+        ));
+  };
+
+  handleChange = e => {
+    const { value } = e.target;
+    this.setState({ groups: [value]});
+  };
+    
+
     onSubmit = e => {
         e.preventDefault();
         const { username, email, password } = this.state;
@@ -47,7 +100,10 @@ export class Register extends Component {
         if(this.state.justRegister) {
             return <Redirect to="/login"/>;
         }
+        
         const { username, email, password } = this.state;
+        const { isAuthenticated } = this.props.auth;
+
         return (
             <div className="col-md-6 m-auto">
               <div className="card card-body mt-5">
@@ -84,6 +140,15 @@ export class Register extends Component {
                     />
                   </div>
                   <div className="form-group">
+                    <label><strong>Group</strong></label>
+                    <select
+                      className="form-control"
+                      name="groups"
+                      onChange={this.handleChange}>
+                        {this.renderGroupOptions()}
+                    </select> 
+                  </div>
+                  <div className="form-group">
                     <button type="submit" className="btn btn-primary">Register</button>
                   </div>
                   <p>
@@ -97,6 +162,7 @@ export class Register extends Component {
 }
 
 const mapStateToProps = state => ({
-    isAuthenticated: state.auth.isAuthenticated
+    isAuthenticated: state.auth.isAuthenticated,
+    auth: state.auth
 });
 export default connect(mapStateToProps, { register })(Register);
